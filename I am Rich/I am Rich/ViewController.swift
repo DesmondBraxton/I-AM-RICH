94efd549cafde33c7e5773e949828a384c5761e9
Desmond Braxton Jr <braxtond232@gmail.com>
2022-08-30 08:58:16 -0400

    Tutorial from Udemy results in a Dummy App

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by MacBook Pro on 8/19/22.
+//
+
+// To change the behavior of our app
+// The inspector changes depending on the file shown
+//  The right hand pane is setting, the left hand pane is a navigation controller
+// THe bottom part is the debug area
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
